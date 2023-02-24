3b1c0b8bcd45b14e7891f3ae34e13c8e769332d5
Yogesh Kirtane <yogeshdkirtane@gmail.com>
2023-02-24 10:37:19 +0530

    Project Upload

---
+++ Employee Management Application/Models/Login.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Employee_Mini_Proj.Models
+{
+    public class Login
+    {
+        [Required(ErrorMessage = "Please enter Username")]
+        [Display(Name = "Username")]
+        public string? Username { get; set; }
+
+        [Required(ErrorMessage = "Please enter password")]
+        [DataType(DataType.Password)]
+        [StringLength(100, ErrorMessage = "Password \"{0}\" must have {2} character", MinimumLength = 8)]
+        [RegularExpression(@"^([a-zA-Z0-9@*#]{8,15})$", ErrorMessage = "Password must contain: Minimum 8 characters atleast 1 UpperCase Alphabet, 1 LowerCase      Alphabet, 1 Number and 1 Special Character")]
+        public string? Password { get; set; }
+    }
+}
