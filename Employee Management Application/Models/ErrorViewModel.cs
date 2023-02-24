db0cc51de0cb2b4291df7e10eddcffe3403bceec
Yogesh Kirtane <113686826+YogeshKirtane@users.noreply.github.com>
2023-02-24 10:17:59 +0530

    Delete Employee Management Application directory

--- Employee Management Application/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Employee_Mini_Proj.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
