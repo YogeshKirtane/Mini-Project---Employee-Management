15e6e7c7f2eaeb5d7af407b50fa6a82fdd219bd0
Yogesh Kirtane <113686826+YogeshKirtane@users.noreply.github.com>
2023-02-24 10:09:08 +0530

    Add files via upload

---
+++ Employee Management Application/Controllers/LoginController.cs
@@ -0,0 +1,36 @@
+﻿using Employee_Mini_Proj.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Employee_Mini_Proj.Controllers
+{
+    public class LoginController : Controller
+    {
+        private readonly MiniprojContext _context;
+
+        public LoginController(MiniprojContext context)
+        {
+            _context = context;
+        }
+
+        // GET: LoginController
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(Login log)
+        {
+            bool isValid = _context.Employees.Any(u => u.Username == log.Username && u.Password == log.Password);
+            if (isValid)
+            {
+                //FormsAuthentication.SetAuthCookie(emp.Username, false);
+                return RedirectToAction("Index", "Employees");
+            }
+
+            ModelState.AddModelError("", "Invalid Username/Password!");
+            return View();
+        }
+    }
+}
---
+++ Employee Management Application/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Employee_Mini_Proj.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
